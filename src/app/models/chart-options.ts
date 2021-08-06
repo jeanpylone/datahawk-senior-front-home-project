a424ab1fb68bda245b6b91cef82d3600362025d9
Jean-Philippe Schneider <jeanpylone@gmail.com>
2021-08-06 16:20:18 +0200

    experiment #8 - Choose wiser colors to represent the different product series

--- src/app/models/chart-options.ts
+++ src/app/models/chart-options.ts
@@ -1,4 +1,31 @@
-import { ChartOptions } from 'chart.js';
+import { ChartOptions, Chart } from 'chart.js';
+
+Chart.plugins.register({
+    afterDatasetUpdate(chartInstance: Chart, options?: any) {
+        const dataset = chartInstance?.data?.datasets?.[options.index];
+        const lastDataIndex = (chartInstance as any).scales["x-axis-0"].ticks.length;
+
+        if(dataset) {
+            const lastData = dataset?.data?.[lastDataIndex];
+            if (lastData) {
+                const value = lastData as number;
+                dataset.pointBackgroundColor =
+                dataset.borderColor = `rgba(${
+                    Math.round(value * Math.log(255))
+                },${
+                  255 - Math.round(value * 2)
+                },${
+                  Math.round(value)
+                },0.7)`;
+            } else {
+                dataset.pointBackgroundColor =
+                dataset.borderColor = `rgba(255,255,255,0.5)`;
+            }
+            dataset.pointBorderWidth = 1;
+            dataset.borderWidth = 2;
+        }
+    }
+});
 
 export const CHART_OPTIONS: ChartOptions = {
     defaultColor: 'black',
